92f7af60dd5f862985fa4688d9f0ccad44c48589
ansonmh <ansonmh@gmail.com>
2018-05-15 16:12:51 +0800

    anson v.4

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { firebaseApp } from '../firebase';
+import '../css/SignUp.css';
 
 class SignUp extends Component {
     constructor(props) {
@@ -12,8 +13,7 @@ class SignUp extends Component {
                 message: ''
             }
         }
-        this.memberType = '';
-        
+
     }
 
     signUp() {
@@ -28,41 +28,68 @@ class SignUp extends Component {
 
     render() {
         return (
-            <div className="form-inline" style={{ margin: '5%' }}>
-                <h2>Sign Up</h2>
-                <div className="form-group">
-                    <input
-                        className="form-control"
-                        type="text"
-                        style={{ marginRight: '5px' }}
-                        placeholder="email"
-                        onChange={event => this.setState({ email: event.target.value })}
-                    /><p></p>
-                    <input
-                        className="form-control"
-                        type="password"
-                        style={{ marginRight: '5px' }}
-                        placeholder="password"
-                        onChange={event => this.setState({ password: event.target.value })}
+            <div class="container">
+                <div class="card bg-light">
+                <div style={{ maxWidth: 250 }}>
+                        <h4 class="card-title mt-3 text-center">Create Account</h4>
+                        <p class="text-center">Get started with your free account</p>
+                        <p>
+                            <a href="" class="btn btn-block btn-twitter"> <i class="fab fa-twitter"></i>   Login via Twitter</a>
+                            <a href="" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
+                        </p>
+                        <p class="divider-text">
+                            <span class="bg-light">OR</span>
+                        </p>
+                        <form>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                                </div>
+                                <input name="" class="form-control" placeholder="Full name" type="text" />
+                            </div>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                                </div>
+                                <input name="" class="form-control" placeholder="Email address" type="email" onChange={event => this.setState({ email: event.target.value })} />
+                            </div>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+                                </div>
+                                <input name="" class="form-control" placeholder="Phone number" type="text" />
+                            </div>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+                                </div>
+                                <select class="form-control">
+                                    <option selected=""> Select membership type</option>
+                                    <option>個人 Personal</option>
+                                    <option>學校 School</option>
+                                    <option>團體 Organization</option>
+                                </select>
+                            </div>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                                </div>
+                                <input class="form-control" placeholder="Create password" type="password" />
+                            </div>
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                                </div>
+                                <input class="form-control" placeholder="Repeat password" type="password" onChange={event => this.setState({ password: event.target.value })} />
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block" onClick={() => this.signUp()}> Create Account  </button>
+                            </div>
+                            <p class="text-center">Have an account? <Link to={'/signin'}>Log In</Link> </p>
+                        </form>
 
-                    /><p></p>
-                    <select value={this.state.value} onChange={this.memberType}  > 
-                        <option value="PersonalMem">個人 Personal</option>
-                        <option value="SchoolMem">學校 School</option>
-                        <option value="OrganMem">團體 Organization</option>
-                    </select>
-                    <div>{ this.memberType }</div>
-                    <p></p>
-                    <button
-                        className="btn btn-primary"
-                        type="button"
-                        onClick={() => this.signUp()}
-                    >
-                        Sign Up
-                    </button>
                 </div>
-                <div>{this.state.error.message}</div>
-                <div><Link to={'/signin'}> Sign in page</Link></div>
+                </div>
             </div>
         )
     }
