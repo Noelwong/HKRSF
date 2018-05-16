10e2b2c8c6b3495742b22830e83b5c0985e80fea
ansonmh <ansonmh@gmail.com>
2018-05-16 13:28:46 +0800

    view of addInfor

---
+++ src/components/AddInfor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { db } from '../firebase';
+
+class AddInfor extends Component {
+    render(){
+        return (
+            <div>Information</div>
+        )
+    }
+    
+}
+export default AddInfor;
