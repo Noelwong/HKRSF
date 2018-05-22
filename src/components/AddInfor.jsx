95c86ba88e10a4551c6c73788e527afcfec024d7
ansonmh <ansonmh@gmail.com>
2018-05-22 19:56:23 +0800

    anson v13

--- src/components/AddInfor.jsx
+++ src/components/AddInfor.jsx
@@ -4,21 +4,30 @@ class AddInfor extends Component {
 
         constructor(props) {
           super(props);
-          this.handleChange = '';
+          this.changepage;
         }
 
+
+
+
     render() {
+
         return (
             <div>
-                <select onChange={this.handleChange} >
-                    <option value="Personal">個人Personal</option>
+                <select id = "typeselect" onChange= {this.changepage} >
+                    <option value ="Personal">個人Personal</option>
                     <option value="School">學校School</option>
                     <option value="Organization">組織Organization</option>
                 </select>
-                <b>{this.handleChange}</b>
+                <b id = "test"></b>
             </div>
+
         )
     }
+    changepage = () => {
+        var x = document.getElementById("typeselect").value;
+        document.getElementById("test").innerHTML = "You selected: " + x;
 
+    }
 }
 export default AddInfor;
--- src/components/Admin/AddCompetition.jsx
+++ src/components/Admin/AddCompetition.jsx
@@ -4,9 +4,9 @@ import { firebaseApp, db } from '../../firebase';
 import { Link } from 'react-router';
 
 class AddCompetition extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.competitionInfor= {
+        this.competitionInfor = {
             competitionName: '',
             date: '',
             time: '',
@@ -15,10 +15,10 @@ class AddCompetition extends Component {
                 message: ''
             }
         }
-        
+
     }
 
-    addCompetition(competitionInfor){
+    addCompetition(competitionInfor) {
         db.collection("competition").doc().set({
             name: this.competitionInfor.competitionName,
             date: this.competitionInfor.date,
@@ -27,37 +27,37 @@ class AddCompetition extends Component {
         })
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <form>
-            Competition name:<br></br>
-            比賽名稱:<br></br>
-            <input type="text" name="competitionName" onChange={event => this.setState({ competitionName: event.target.value })} />
-            <br></br>
-            Date:<br></br>
-            日期:<br></br>
-            <input type="text" name="date" onChange={event => this.setState({ date: event.target.value })}/>
-            <br></br>
-            Time:<br></br>
-            時間:<br></br>
-            <input type="text" name="time" onChange={event => this.setState({ time: event.target.value })}/>
-            <br></br>
-            Location:<br></br>
-            地點:<br></br>
-            <input type="text" name="location" onChange={event => this.setState({ location: event.target.value })}/>
-          </form>
-          <button
-                        className="btn btn-success"
-                        type="button"
-                        onClick={() => this.addCompetition(this.competitionInfor)}
-                    >
-                        Submit
+                    Competition name:<br></br>
+                    比賽名稱:<br></br>
+                    <input type="text" id="competitionName" placeholder="Competition Name" onChange={event => this.setState({ competitionName: event.target.value })} />
+                    <br></br>
+                    Date:<br></br>
+                    日期:<br></br>
+                    <input type="text" id="date" placeholder="date" onChange={event => this.setState({ date: event.target.value })} />
+                    <br></br>
+                    Time:<br></br>
+                    時間:<br></br>
+                    <input type="text" id="time" placeholder="time" onChange={event => this.setState({ time: event.target.value })} />
+                    <br></br>
+                    Location:<br></br>
+                    地點:<br></br>
+                    <input type="text" id="location"placeholder="location" onChange={event => this.setState({ location: event.target.value })} />
+                </form>
+                <button
+                    className="btn btn-success"
+                    type="button"
+                    onClick={() => this.addCompetition(this.competitionInfor)}
+                >
+                    Submit
                     </button>
-          </div>
+            </div>
         )
-                
+
     }
-    
+
 }
 export default AddCompetition;
