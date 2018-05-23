1462596109c51b8e269ff765e3a055b0ac8829aa
ansonmh <ansonmh@gmail.com>
2018-05-23 10:01:33 +0800

    alex v1

--- src/components/AddInfor.jsx
+++ src/components/AddInfor.jsx
@@ -8,27 +8,63 @@ class AddInfor extends Component {
         }
 
 
-
-
     render() {
 
         return (
-            <div>
-                <select id = "typeselect" onChange= {this.changepage} >
-                    <option value ="">please select</option>
+            <div class = "pickerdiv">
+                <select id = "typeselect" onChange= {this.changepage } >
+                    <option value ="dfv">Please Select Your User type</option>
                     <option value ="Personal">個人Personal</option>
                     <option value="School">學校School</option>
                     <option value="Organization">組織Organization</option>
                 </select>
-                <b id = "test"></b>
+                <b id = "test">"You selected: Personal"</b>
+
+                    //form for user input
+                <form id="form1" name="form1" /*style="display:none;"*/ /* ERROR will disable all the element */ >
+                    <p>  First name :<input name="name" type="text" id="name" /></p>
+                    <p>Last name :<input name="address" type="text" id="address"  /></p>
+                    <p> Age: <input name="age" type="text" id="age" /></p>
+                </form>
+
             </div>
+
+
 
         )
     }
     changepage = () => {
         var x = document.getElementById("typeselect").value;
         document.getElementById("test").innerHTML = "You selected: " + x;
+console.log(x);
+        if(x == "Personal"){
+            var createinput = document.createElement("input");
+            createinput.setAttribute('type', 'text');
+            document.body.appendChild(createinput);
+
+           //set visable show/hidden
+            document.getElementById("form1").style.display="none";
+        }else if (x =="School"){
+
+        }else if (x =="Organization" ){
+
+        }
+
+
+
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
 }
 export default AddInfor;
