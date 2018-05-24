9307cd05f285e1fca9afdddc1e1406b4b02a7cc1
ansonmh <ansonmh@gmail.com>
2018-05-24 21:19:15 +0800

    anson V19.5

--- src/components/Personal/PersonalInfor.jsx
+++ src/components/Personal/PersonalInfor.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { firebaseApp } from '../../firebase';
+import { firebaseApp,db } from '../../firebase';
 
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
@@ -15,7 +15,6 @@ class PersonalInfor extends Component {
             EName: '',
             BDate: moment(),
             ID: '',
-            email:'',
             home:'',
             schoolType:'',
             schoolName:'',
@@ -28,14 +27,33 @@ class PersonalInfor extends Component {
                 message: ''
             }
         }
+        this.user = firebaseApp.auth().currentUser;
+
         this.handleChange = this.handleChange.bind(this);
+        this.schoolTypeHandleChange = this.schoolTypeHandleChange.bind(this);
+        this.coachLevelHandleChange = this.coachLevelHandleChange.bind(this);
+        this.judgeLevelHandleChange = this.judgeLevelHandleChange.bind(this);
 
     }
+    
+
 
     handleChange(date) {
         this.setState({
             BDate: date,
         });
+      }
+    
+      schoolTypeHandleChange(event) {
+        this.setState({schoolType: event.target.value});
+      }
+
+      coachLevelHandleChange(event) {
+        this.setState({coachLevel: event.target.value});
+      }
+
+      judgeLevelHandleChange(event) {
+        this.setState({judgeLevel: event.target.value});
       }
 
     render(){
@@ -49,11 +67,13 @@ class PersonalInfor extends Component {
                 <input type="text"
                      id="CName" 
                      placeholder="姓名"  
+                     onChange={event => this.setState({ CName: event.target.value })}
                      /><br/>
                 Name (English)
                 <input type="text"
                     id="EName" 
                     placeholder="Name"  
+                    onChange={event => this.setState({ EName: event.target.value })}
                     /><br/>
                 出生日期<br/>
                 Date of Birth
@@ -66,41 +86,39 @@ class PersonalInfor extends Component {
                 <input type="text"
                      id="ID" 
                      placeholder="ID Number"  
-                     /><br/>
-                電郵地址<br/>
-                Email Address
-                <input type="text"
-                     id="email" 
-                     placeholder="Email Addressr"  
+                     onChange={event => this.setState({ ID: event.target.value })}
                      /><br/>
                 住宅地址<br/>
                 Home Address
                 <input type="text"
                      id="home" 
                      placeholder="Home Addressr"  
+                     onChange={event => this.setState({ home: event.target.value })}
                      /><br/>
                 就讀學校<br/>
                 Current school
-                <select id = "schoolType" >
-                    <option value ="dfv">Please Select Your School type</option>
+                <select id = "schoolType" value={this.state.schoolType} onChange={this.schoolTypeHandleChange} >
+                    <option value ="">Please Select Your School type</option>
                     <option value ="PSchool">小學Primary School</option>
                     <option value="SSchool">中學Secondary Schooll</option>
                     <option value="College">大專College</option>
                 </select>
                 <input type="text"
                      id="schoolName" 
-                     placeholder="School Name"  
+                     placeholder="School Name" 
+                     onChange={event => this.setState({ schoolName: event.target.value })} 
                      /><br/>
                 所屬體育團體名稱<br/>
                 The name of the sports group
                 <input type="text"
                      id="sGroup" 
                      placeholder="Name"  
+                     onChange={event => this.setState({ sGroup: event.target.value })} 
                      /><br/>
                 教練資歷及考獲年份<br/>
                 Coaching qualifications and years of study
-                <select id = "coachLevel" >
-                    <option value ="No">沒有 NULL</option>
+                <select id = "coachLevel" value={this.state.coachLevel} onChange={this.coachLevelHandleChange}>
+                    <option value ="">沒有 NULL</option>
                     <option value ="Junior">初級Junior</option>
                     <option value="Intermediate">中級Intermediate</option>
                     <option value="Advanced">高級Advanced</option>
@@ -109,11 +127,12 @@ class PersonalInfor extends Component {
                 <input type="text"
                      id="coachYear" 
                      placeholder="Years of study"  
+                     onChange={event => this.setState({ coachYear: event.target.value })} 
                      /><br/>
                 裁判資歷及考獲年份<br/>
                 Judge qualifications and years of study
-                <select id = "judgeLevel" >
-                    <option value ="No">沒有 NULL</option>
+                <select id = "judgeLevel" value={this.state.judgeLevel} onChange={this.judgeLevelHandleChange}>
+                    <option value ="">沒有 NULL</option>
                     <option value ="Junior">初級Junior</option>
                     <option value="Intermediate">中級Intermediate</option>
                     <option value="Advanced">高級Advanced</option>
@@ -121,9 +140,16 @@ class PersonalInfor extends Component {
                 </select>
                 <input type="text"
                      id="judgeYear" 
-                     placeholder="Years of study"  
+                     placeholder="Years of study" 
+                     onChange={event => this.setState({ judgeYear: event.target.value })}  
                      /><br/>
                 </form>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.addPersonalInfor(this.state)}    
+                >
+                    Submit
+                </button>
                 <button
                     className="btn btn-danger"
                     onClick={() => this.signOut()}
@@ -135,6 +161,25 @@ class PersonalInfor extends Component {
     }
     signOut() {
         firebaseApp.auth().signOut();
+    }
+
+    addPersonalInfor(state) {
+        const uid = this.user.uid;
+        db.collection("user").doc(uid).set({
+            userType:'Personal',
+            CName: this.state.CName,
+            EName: this.state.EName,
+            BDate: new Date(this.state.BDate),
+            ID: this.state.ID,
+            home: this.state.home,
+            schoolType: this.state.schoolType,
+            schoolName: this.state.schoolName,
+            sGroup: this.state.sGroup,
+            coachLevel: this.state.coachLevel,
+            coachYear: this.state.coachYear,
+            judgeLevel: this.state.judgeLevel,
+            judgeYear: this.state.judgeYear
+        });
     }
     
 }
