76dbdebe13bffaba09add75b24dd4023c09c08f0
ansonmh <ansonmh@gmail.com>
2018-05-25 23:54:30 +0800

    anson V21.2.1

--- src/components/Admin/AdminHome.jsx
+++ src/components/Admin/AdminHome.jsx
@@ -33,25 +33,25 @@ class AdminHome extends Component {
             <div>
                 AdminHome
                 <br/>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active" onClick={() => this.handleChange()}><a>Add Competition</a></li>
-                        <li class="active" onClick={() => this.handleChangeShow()}><a>Show Competition</a></li>
+                <div id="navbar" className="navbar-collapse collapse">
+                    <ul className="nav navbar-nav">
+                        <li className="active" onClick={() => this.handleChange()}><a>Add Competition</a></li>
+                        <li className="active" onClick={() => this.handleChangeShow()}><a>Show Competition</a></li>
                         <li><a href="">Rule Setting</a></li>
-                        <li class="dropdown">
-                            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
+                        <li className="dropdown">
+                            <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                            <ul className="dropdown-menu">
                                 <li><a href="">Action</a></li>
                                 <li><a href="">Another action</a></li>
                                 <li><a href="">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
+                                <li role="separator" className="divider"></li>
+                                <li className="dropdown-header">Nav header</li>
                                 <li><a href="">Separated link</a></li>
                                 <li><a href="">One more separated link</a></li>
                             </ul>
                         </li>
                     </ul>
-                    <ul class="nav navbar-nav navbar-right">
+                    <ul className="nav navbar-nav navbar-right">
                         <li><a href="../navbar/">Default</a></li>
                         <li><a href="../navbar-static-top/">Static top</a></li>
                         <li><button
--- src/components/Admin/showComp.jsx
+++ src/components/Admin/showComp.jsx
@@ -4,25 +4,28 @@ import { db } from '../../firebase';
 class ShowComp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+        }
         this.getCompInfor();
     }
-    getCompInfor() {
-
-        var citiesRef = db.collection('competition');
-        var allCities = citiesRef.get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    console.log(doc.id, '=>', doc.data().name);
-                });
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
+getCompInfor(){
+    this.com = new Array();
+    db.collection('competition').get().then(snapshot => 
+        {snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data().name);
+                this.com.push(doc.id,doc.data().name);
+                console.log(this.com);
             });
-    }
+        }) 
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
+}
 
     render() {
         return (
-            <div>1234</div>
+           <div>{this.com}</div>
+           
         )
 
     }
