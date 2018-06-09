1646cd13a6ff55accd3365b7d546c0c95d353b77
ansonmh <ansonmh@gmail.com>
2018-06-09 17:37:51 +0800

    anson V24.2(navbar)

--- src/components/Admin/AdminHome.jsx
+++ src/components/Admin/AdminHome.jsx
@@ -1,30 +1,52 @@
 import React, { Component } from 'react';
 import { firebaseApp } from '../../firebase';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 import CompetitionBar from './CompetitionBar';
 import FormatSetting from './FormatSetting';
+import AddCompetition from './AddCompetition';
+import ShowComp from './ShowComp';
+import AddCompItem from './AddCompItem'
 
 class AdminHome extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            showContent:''
+        this.state = {
+            showContent: ''
         }
     }
     handleChange() {
-        this.setState({showContent:'CompetitionBar'})
-      }
+        this.setState({ showContent: 'CompetitionBar' })
+    }
 
-      handleChangeFormat() {
-        this.setState({showContent:'FormatSetting'})
-      }
+    handleChangeFormat() {
+        this.setState({ showContent: 'FormatSetting' })
+    }
 
-    selectShowContent = (showContent) =>{
-        if(showContent != null){
-            if(showContent === 'CompetitionBar'){
-                return(<CompetitionBar/>)
-            }else if(showContent === 'FormatSetting'){
-                return(<FormatSetting/>)
+    handleChangeAddcomp() {
+        this.setState({ showContent: 'AddCompetition' })
+    }
+
+    handleChangeShow() {
+        this.setState({ showContent: 'ShowCompetition' })
+    }
+
+    handleChangeAdd() {
+        this.setState({ showContent: 'AddCompItem' })
+    }
+
+    selectShowContent = (showContent) => {
+        if (showContent != null) {
+            if (showContent === 'CompetitionBar') {
+                return (<CompetitionBar />)
+            } else if (showContent === 'FormatSetting') {
+                return (<FormatSetting />)
+            } else if (showContent === 'AddCompetition') {
+                return (<AddCompetition />)
+            } else if (showContent === 'ShowCompetition') {
+                return (<ShowComp />)
+            } else if (showContent === 'AddCompItem') {
+                return (<AddCompItem />)
             }
         }
     }
@@ -33,24 +55,35 @@ class AdminHome extends Component {
         return (
             <div>
                 AdminHome
-                <br/>
-                <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav">
-                        <li className="active" onClick={() => this.handleChange()}><a>比賽<br/>Competition</a></li>
-                        <li className="active" onClick={() => this.handleChangeFormat()}><a>Format Setting</a></li>
-                        <li><a>Rule Setting</a></li>
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><a href="https://us-central1-hkrsf-csci321.cloudfunctions.net/readInfo?id=Daddy">Test</a></li>
-                        <li><button
-                            className="btn btn-outline-warning"
-                            onClick={() => this.signOut()}
-                        >
-                            登出<br/>Sign Out
-                        </button></li>
-                    </ul>
-                </div>
-                <br/>
+                <br />
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a>AdminHome</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1}>
+                            Link
+                     </NavItem>
+                        <NavItem eventKey={2}>
+                            Link
+                    </NavItem>
+                        <NavDropdown eventKey={3} title="比賽Competition" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1} onClick={() => this.handleChangeAddcomp()}>Add Competition</MenuItem>
+                            <MenuItem eventKey={3.2} onClick={() => this.handleChangeShow()}>Show Competition</MenuItem>
+                            <MenuItem eventKey={3.3} onClick={() => this.handleChangeAdd()}>Add item</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={4} onClick={() => this.signOut()}>
+                        登出<br />Sign Out
+                     </NavItem>
+                    </Nav>
+                </Navbar>
+                <br />
                 {this.selectShowContent(this.state.showContent)}
             </div >
 
