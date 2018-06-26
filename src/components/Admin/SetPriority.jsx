6f6bbaf1171ca73a85e2880f746f0515fd995868
JamesQ1u <37471584+JamesQ1u@users.noreply.github.com>
2018-06-26 20:50:08 +0800

    anson v28(stable !! Remember 【npm start】 before use)

--- src/components/Admin/SetPriority.jsx
+++ src/components/Admin/SetPriority.jsx
@@ -1,42 +1,43 @@
 import React, { Component } from 'react';
 import { db } from '../../firebase';
+import { Table } from 'react-bootstrap';
 
 class SetPriority extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rank:[''],
-            district:[],
-            groupType:[],
-            itemType:[],
-            numOfPeople:[],
-            timeLimit:[],
-            District:'',
-            GroupType:'',
-            ItemType:'',
-            NumOfPeople:'',
-            TimeLimit:'',
-            url:'https://us-central1-hkrsf-csci321.cloudfunctions.net/scheduling_test?arr_1='
+            rank: [''],
+            district: [],
+            groupType: [],
+            itemType: [],
+            numOfPeople: [],
+            timeLimit: [],
+            District: '',
+            GroupType: '',
+            ItemType: '',
+            NumOfPeople: '',
+            TimeLimit: '',
+            url: 'https://us-central1-hkrsf-csci321.cloudfunctions.net/scheduling_test?arr_1='
         }
         this.Ref = db.collection('competition').doc(sessionStorage.compID).collection('competitionItem');
         this.getAll();
     }
 
-    getAll(){
-        this.Ref.onSnapshot(function(querySnapshot) {
+    getAll() {
+        this.Ref.onSnapshot(function (querySnapshot) {
             var district = [];
             var groupType = [];
             var itemType = [];
             var numOfPeople = [];
             var timeLimit = [];
-            querySnapshot.forEach(function(doc) {
+            querySnapshot.forEach(function (doc) {
                 district.push(doc.data().district);
                 groupType.push(doc.data().groupType);
                 itemType.push(doc.data().itemType);
                 numOfPeople.push(doc.data().numOfPeople);
                 timeLimit.push(doc.data().timeLimit);
             });
-           
+
             district = Array.from(new Set(district));
             itemType = Array.from(new Set(itemType));
             numOfPeople = Array.from(new Set(numOfPeople));
@@ -49,70 +50,91 @@ class SetPriority extends Component {
             console.log(timeLimit);
             console.log(groupType);
         });
-        
+
     }
 
-    setRank(){
-        this.setState({rank:[ this.state.District, this.state.GroupType, this.state.ItemType, this.state.NumOfPeople, this.state.TimeLimit]});
+    setRank() {
+        this.setState({ rank: [this.state.District, this.state.GroupType, this.state.ItemType, this.state.NumOfPeople, this.state.TimeLimit] });
     }
 
 
     render() {
         return (
             <div>
-                地區:<br/>
+                <Table striped bordered condensed hover>
+                    <thead>
+                        <tr>
+                            <th>地區:<br />
                 District:
                 <input type="number"
-                     id="District" 
-                     min="1" max="5"
-                     onChange={event => this.setState({ District: event.target.value })}
-                     />
-                <br/>
-                比賽項目:<br/>
+                    id="District"
+                    min="1" max="5"
+                    onChange={event => this.setState({ District: event.target.value })}
+                /></th>
+                            <th>比賽項目:<br />
                 Item Type:
                 <input type="number"
-                     id="ItemType" 
-                     min="1" max="5"
-                     onChange={event => this.setState({ ItemType: event.target.value })}
-                     />
-                <br/>
-                參賽人數:<br/>
+                    id="ItemType"
+                    min="1" max="5"
+                    onChange={event => this.setState({ ItemType: event.target.value })}
+                /></th>
+                            <th>參賽人數:<br />
                 # Of People:
                 <input type="number"
-                     id="NumOfPeople" 
-                     min="1" max="5"
-                     onChange={event => this.setState({ NumOfPeople: event.target.value })}
-                     />
-                <br/>
-                時間限制:<br/>
+                    id="NumOfPeople"
+                    min="1" max="5"
+                    onChange={event => this.setState({ NumOfPeople: event.target.value })}
+                /></th>
+                            <th>時間限制:<br />
                 Time Limit:
                 <input type="number"
-                     id="TimeLimit" 
-                     min="1" max="5"
-                     onChange={event => this.setState({ TimeLimit: event.target.value })}
-                     />
-                <br/>
-                組別:<br/>
+                    id="TimeLimit"
+                    min="1" max="5"
+                    onChange={event => this.setState({ TimeLimit: event.target.value })}
+                /></th>
+                <th> 組別:<br />
                 Group Type:
                 <input type="number"
-                     id="GroupType" 
-                     min="1" max="5"
-                     onChange={event => this.setState({ GroupType: event.target.value })}
-                     />
-                     <br/>
-                <a target="_blank" href={this.state.url+ JSON.stringify(this.state.rank)}><button
+                    id="GroupType"
+                    min="1" max="5"
+                    onChange={event => this.setState({ GroupType: event.target.value })}
+                /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>;
+                     <br />
+                <a target="_blank" href={this.state.url + JSON.stringify(this.state.rank)}><button
                     className="btn btn-success"
                     onClick={() => this.setRank()}
                 >
                     Submit
                 </button></a>
-                <br/>
-                <br/>
+                <br />
+                <br />
             </div>
         )
 
     }
 
-    
+
 }
 export default SetPriority;
