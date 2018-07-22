25cdb2ec27cdb14c28610fedbb3cc10f2cbaa1ab
NorthWind <lemon97213@gmail.com>
2018-07-22 22:50:05 +0800

    shows

---
+++ src/components/Admin/ShowScore.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { db } from '../../firebase';
+import { Alert, Button } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Column, Row } from 'simple-flexbox';
+
+
+class ShowScore extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+
+
+    render(){
+
+        return(
+            <div>
+                hi
+
+            </div>
+        )
+    }
+
+}
+export default ShowScore;
