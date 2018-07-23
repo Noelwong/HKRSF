9d11f217cfc5b57e6f45d4476d5c1e6b45602610
NorthWind <lemon97213@gmail.com>
2018-07-23 16:55:04 +0800

    no message

--- src/components/Admin/ShowScore.jsx
+++ src/components/Admin/ShowScore.jsx
@@ -25,22 +25,16 @@ class ShowScore extends Component {
         this.Ref = db.collection('competition').doc(sessionStorage.compID);
         this.handleSelectComp = this.handleSelectComp.bind(this);
         this.handleShowScore = this.handleShowScore.bind(this);
+        this.sortButton = this.sortButton.bind(this);
+        this.reloadB = this.reloadB.bind(this);
         this.getAll();
     }
-
+    
     getAll() {
         let tempCompItem = sessionStorage.getItem("compItem");
         // eslint-disable-next-line
         this.state.allCompItem = JSON.parse(tempCompItem);
-        //let tempParticipantID = sessionStorage.getItem("participantSetID");
-        //const setPID = JSON.parse(tempParticipantID);
-        //let tempParticipantName = sessionStorage.getItem("participantSetName");
-        //const setPName = JSON.parse(tempParticipantName);
-        // eslint-disable-next-line
-        // this.state.selected = setPID.map((k, i) => ({
-        //     id: setPID[i],
-        //     content: setPName[i]
-        // }))
+
         let ArrayOfParticipantInItem =[];
         db.collection('competition').doc(sessionStorage.compID).collection('competitionItem').onSnapshot(coll => {
             coll.forEach(doc=>{
@@ -71,59 +65,7 @@ class ShowScore extends Component {
             this.state.ArrayOfParticipantInItem =ArrayOfParticipantInItem;
             console.log(this.state.ArrayOfParticipantInItem);
             // console.log(ArrayOfParticipantInItem);
-        })
-
-
-        //Sorting
-
-        let tempMarks = 0;
-        let tempMarkSet ={};
-        // this.Ref.collection('competitionItem').
-        // doc(this.state.allCompItem[index]).
-        // collection('participantCollection').
-        // // where("ParticipantName","==",name).
-        // get().
-        // then(snapshot =>{
-        //     snapshot.forEach(doc =>{
-        //         tempMarks =doc.data().TotalMark;
-        //         alert("In Competiton : "+
-        //             this.state.allCompItem[index] +
-        //             "  Participant: " +
-        //             name +
-        //             "\n GET  " +
-        //             " " +
-        //             doc.data().TotalMark +
-        //             " Marks");
-        //         console.log(doc.data().TotalMark);
-        //
-        //         const pMarks = {
-        //             [name]:doc.data().TotalMark
-        //         }
-        //
-        //         tempMarkSet[doc.data().ParticipantName] = doc.data().TotalMark;
-        //     })
-        //
-        //     let sortTable =[];
-        //     for ( let pName in tempMarkSet){
-        //         sortTable.push([pName,tempMarkSet[pName]]);
-        //     }
-        //     sortTable.sort(function(a, b) {
-        //         return b[1] - a[1];
-        //     });
-        //
-        //     console.log(tempMarkSet);
-        //     console.log(sortTable);
-        // })
-
-
-
-
-
-
-
-
-
-
+        });
 
 
 
@@ -161,7 +103,7 @@ class ShowScore extends Component {
 
     handleShowScore(name, index){
         let tempMarks = 0;
-
+        let tempMarkSet ={};
         this.Ref.collection('competitionItem').
         doc(this.state.allCompItem[index]).
         collection('participantCollection').
@@ -179,13 +121,59 @@ class ShowScore extends Component {
                     doc.data().TotalMark +
                     " Marks");
                 console.log(doc.data().TotalMark);
-
+                tempMarkSet[doc.data().ParticipantName] = doc.data().TotalMark;
             });
 
+            let sortTable =[];
+            for ( let pName in tempMarkSet){
+                sortTable.push([pName,tempMarkSet[pName]]);
+            }
+            sortTable.sort(function(a, b) {
+                return b[1] - a[1];
+            });
+
+            console.log(tempMarkSet);
+            console.log(sortTable);
         })
+
 
     }
 
+    sortButton(index){
+        let tempMarks = 0;
+        let tempMarkSet ={};
+        this.Ref.collection('competitionItem').
+        doc(this.state.allCompItem[0]).
+        collection('participantCollection').
+        //where("ParticipantName","==",name).
+        get().
+        then(snapshot =>{
+            snapshot.forEach(doc =>{
+                tempMarks =doc.data().TotalMark;
+                console.log(doc.data().TotalMark);
+                tempMarkSet[doc.data().ParticipantName] = doc.data().TotalMark;
+            });
+
+            let sortTable =[];
+            for ( let pName in tempMarkSet){
+                sortTable.push([pName,tempMarkSet[pName]]);
+            }
+            sortTable.sort(function(a, b) {
+                return b[1] - a[1];
+            });
+
+            console.log(tempMarkSet);
+            console.log(sortTable);
+        })
+
+
+    }
+
+
+    reloadB(){
+
+        window.location.reload();
+    }
 
     render(){
 
@@ -194,16 +182,13 @@ class ShowScore extends Component {
                 <Column flexGrow={1}>
                     <Row horizontal='center'>
                         <Column>
-                            <h4>Select Competition and Athletes</h4>
+
+                            <h4> Show All Competitions and Athletes</h4>
+
                         </Column>
                     </Row>
 
-                    <Row horizontal='center'>
-                        <h2>{this.state.marks}</h2>
-
-                    </Row>
                     <Row >
-
 
                         <Column flexGrow={0} horizontal='center'>
 
@@ -213,7 +198,7 @@ class ShowScore extends Component {
 
                                     <ListGroupItem >
                                         {topic.map((name,i)=>
-                                        <Button key = {name+this.state.ArrayOfParticipantInItem[index]} onClick={() => this.handleShowScore(name,index)}>{name}</Button>
+                                        <Button key = {name+this.state.ArrayOfParticipantInItem[index]+name} onClick={() => this.handleShowScore(name,index)}>{name}</Button>
                                     )
                                         }</ListGroupItem>
                                     </ListGroup>
